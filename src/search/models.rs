4b9908d1e9cd04007d887acdb8516f41c43732ed
Elliot Pahel-Short <robert@pretension.io>
2024-10-28 11:17:09 -0400

    feat: Add Serialize derive to SearchResult, SerpApiResponse, and SearchMetadata

--- src/search/models.rs
+++ src/search/models.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct SearchResult {
     pub title: String,
     pub link: String,
@@ -8,13 +8,13 @@ pub struct SearchResult {
     pub position: Option<i32>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct SerpApiResponse {
     pub search_metadata: SearchMetadata,
     pub organic_results: Vec<SearchResult>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct SearchMetadata {
     pub status: String,
     pub total_time_taken: f32,
